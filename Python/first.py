0376464e5f6b5cce2c76f246a7a7982d4b58a783
ahmadshah24 <ahmadshahqand@gmail.com>
2026-05-13 18:12:46 -0400

    add first python file and some code

--- Python/first.py
+++ Python/first.py
@@ -1,1 +1,55 @@
 print("Hello, World!")
+print("Welcome to Python programming.")
+print("this is Sayed Ahmad shah sekandary")
+
+# when we want to have note or a code will be used in the futrue we can use the comment 
+# comments are run the and can not read that 
+
+
+# data type
+
+age = 24 # intergaer
+name = "sayed" # string
+cgpa = 3.4 # float
+
+#python is dynamically typed language
+print(type(cgpa))
+print(type(age))
+print(type(name))
+
+a = 10
+b = "13"
+
+# print(a + b) # error TypeError: unsupported operand type(s) for +: 'int' and 'str'
+print(a+ int(b))
+
+
+#user input
+a = input("Enter a number")
+print("you have entered", a)
+
+b = input("Enter a word")
+print("you have entered", )
+
+'''
+this is multi line comment
+
+'''
+
+# excape sequences
+print("hello work \n \twe are here to learn more")
+
+print("hello work",  sep ="") #we can you sep = to change the default behaivor of the print
+print("this is we here to learn more")
+print("this is another line of code" , end="")
+
+print("this is another line with in the same line")
+# python in terminal is REPL, read, evaluate, pring loop 
+print("-----------------python ooperators-------------------------")
+
+
+
+
+
+
+
